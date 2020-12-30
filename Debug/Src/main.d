17717f2e0178c4b54af56d095680281ee388bc58
Sorrasit Bunluehan <b.sorrasit@gmail.com>
2020-12-30 14:11:59 +0100

    .

--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -1,19 +1,19 @@
 Src/main.o: ../Src/main.cpp \
- D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/stm32f446re.h \
- D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/gpio_driver.h \
- D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/stm32f446re.h \
- D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/uart_driver.h \
- D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/rcc_driver.h \
- D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/spi_driver.h
+ D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/stm32f446re.h \
+ D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/gpio_driver.h \
+ D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/stm32f446re.h \
+ D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/uart_driver.h \
+ D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/rcc_driver.h \
+ D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/spi_driver.h
 
-D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/stm32f446re.h:
+D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/stm32f446re.h:
 
-D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/gpio_driver.h:
+D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/gpio_driver.h:
 
-D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/stm32f446re.h:
+D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/stm32f446re.h:
 
-D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/uart_driver.h:
+D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/uart_driver.h:
 
-D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/rcc_driver.h:
+D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/rcc_driver.h:
 
-D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs/spi_driver.h:
+D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs/spi_driver.h:
--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -25,7 +25,7 @@ CPP_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/main.o: ../Src/main.cpp
-	arm-none-eabi-g++ "$<" -mcpu=cortex-m4 -std=gnu++14 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver" -I"D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/My_Driver/incs" -I"D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_CPP/Inc" -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"Src/main.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m4 -std=gnu++14 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver" -I"D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/My_Driver/incs" -I"D:/Self-Study/MyProj/STM32F446RE/STM32F446RE_BME280/Inc" -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"Src/main.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 Src/syscalls.o: ../Src/syscalls.c
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/syscalls.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 Src/sysmem.o: ../Src/sysmem.c
